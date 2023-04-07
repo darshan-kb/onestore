ef8b146d6fb69881dde6894d99d777ac457c9e31
Darshan <darshan.kb@outlook.com>
2023-04-08 02:51:04 +0530

    new project created

---
+++ src/main/java/com/application/oneestore/OneestoreApplication.java
@@ -0,0 +1,13 @@
+package com.application.oneestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneestoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/oneestore/OneestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.oneestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
