551d8298c78f9e43803ed740f1c94bad7c9f77b1
Darshan <darshan.kb@outlook.com>
2023-04-08 15:01:36 +0530

    Mapping corrected

--- src/main/java/com/application/oneestore/entites/Categories.java
+++ src/main/java/com/application/oneestore/entites/Categories.java
@@ -1,15 +1,28 @@
 package com.application.oneestore.entites;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
+@Entity
 public class Categories {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long category_ID;
 	private String cat_1;
 	private String cat_2;
+	
+	
+	
+	@OneToMany(mappedBy = "product_ID")
+	private List<Products> products;
+	
 	public long getCategory_ID() {
 		return category_ID;
 	}
@@ -28,11 +41,19 @@ public class Categories {
 	public void setCat_2(String cat_2) {
 		this.cat_2 = cat_2;
 	}
-	public Categories(long category_ID, String cat_1, String cat_2) {
+	
+	public List<Products> getProducts() {
+		return products;
+	}
+	public void setProducts(List<Products> products) {
+		this.products = products;
+	}
+	public Categories(long category_ID, String cat_1, String cat_2, List<Products> products) {
 		super();
 		this.category_ID = category_ID;
 		this.cat_1 = cat_1;
 		this.cat_2 = cat_2;
+		this.products = products;
 	}
 	public Categories() {
 		super();
